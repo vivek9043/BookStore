ce4e9143781ddfcece68e78951e9a24bd0873548
vivek9043 <77523949+vivek9043@users.noreply.github.com>
2021-02-23 23:10:12 +0530

    Added MVC Feature in Empty Project

--- Vivek.BookStore/Startup.cs
+++ Vivek.BookStore/Startup.cs
@@ -16,6 +16,8 @@ namespace Vivek.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,50 @@ namespace Vivek.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            // Try Middle Ware Concept
+
+            //app.Use(async (context,next)=>
+            //{
+
+            //    await context.Response.WriteAsync("Hello From My First Middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello From My First Middleware Response");
+            //}
+            //);
+
+            //app.Use(async (Context, next) =>
+            //{
+            //    await Context.Response.WriteAsync("Hello From My Second Middleware");
+            //});
+
+            //app.Use(async(context,next)=>
+            //    {
+            //        await context.Response.WriteAsync("Hello From My Third Middleware");
+            //        await next();
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+
+                endpoints.MapDefaultControllerRoute();
+
+                // Now Add Librerary of MVC in Current Application
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Vivek!");
+            //    });
+            //});
         }
     }
 }
